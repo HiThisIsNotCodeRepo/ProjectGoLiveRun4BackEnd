3242cb6bb1c9b53e07a4678c02a501dadcaaaacb
qinchenfeng <freddy.qin@gmail.com>
2021-07-02 00:07:33 +0800

    save work

--- installation/sql/mysql.sql
+++ installation/sql/mysql.sql
@@ -12,6 +12,13 @@ CREATE TABLE user
     last_login DATETIME COMMENT 'user last login datetime',
     PRIMARY KEY (uid)
 );
+drop table if exists category;
+CREATE TABLE category
+(
+    cid TINYINT UNSIGNED COMMENT 'category id',
+    title VARCHAR(20) COMMENT 'category title',
+    PRIMARY KEY(cid)
+);
 drop table if exists task;
 CREATE TABLE task
 (
@@ -30,22 +37,22 @@ CREATE TABLE task
     task_owner_rate SMALLINT UNSIGNED COMMENT 'task owner expected rate 0~65535 required',
     task_deliver_id VARCHAR(36) COMMENT 'task deliver user id UUID',
     task_deliver_rate SMALLINT UNSIGNED COMMENT 'task deliver rate, final rate 0~65535',
-    PRIMARY KEY (task_id)
+    PRIMARY KEY (task_id),
+    FOREIGN KEY (task_category_id) REFERENCES category(cid),
+    FOREIGN KEY (task_owner_id) REFERENCES user(uid),
+    FOREIGN KEY (task_deliver_id) REFERENCES user(uid)
 );
+drop table if exists task_bid;
 CREATE TABLE task_bid
 (
     task_id VARCHAR(36) COMMENT 'task id UUID',
     task_bidder_id VARCHAR(36) COMMENT 'task bidder id UUID',
     task_bidder_rate SMALLINT UNSIGNED COMMENT 'task bidder rate 0~65535',
     INDEX(task_id),
-    Index(task_bidder_id)
+    Index(task_bidder_id),
+    FOREIGN KEY (task_id) REFERENCES task(task_id)
 );
-CREATE TABLE category
-(
-    cid TINYINT UNSIGNED COMMENT 'category id',
-    title VARCHAR(20) COMMENT 'category title',
-    PRIMARY KEY(cid)
-);
+
 CREATE USER 'user'@'%' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON  paotui.* TO 'user'@'%';
 FLUSH PRIVILEGES;
